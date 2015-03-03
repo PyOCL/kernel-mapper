2a8b922c10d739858d0766d1f531896c050acdbf
Kilik <kilik.kuo@gmail.com>
2015-03-04 00:30:51 +0800

    Add method definition generator and some sample for test

--- oclPyObjGenerator.py
+++ oclPyObjGenerator.py
@@ -13,14 +13,36 @@ def getClassDef(strName, inheritCls=[]):
     return strCls
 
 def getMethodDef(strMethodName, dicArgd={}):
-    strREt = 'def %s(self, '%(strMethodName)
+    strRetHead = 'def %s'%(strMethodName)
+    strParams = '(self, '
+    lstVars = dicArgd.get('var', [])
+    lstVars_Default = dicArgd.get('var_default', [])
+    args = dicArgd.get('args', None)
+    argd = dicArgd.get('argd', None)
+
+    for var in lstVars:
+        strParams += var + ', '
+    for dicVar in lstVars_Default:
+        for var, default in dicVar.iteritems():
+            strTemp = var + '=' + str(default) + ', '
+            strParams += strTemp
+    if args:
+        strParams += '*args, '
+    if argd:
+        strParams += '**argd, '
+    strParams = strParams[:-2]
+
+    strRetTail = '):' + os.linesep
+    return strRetHead + strParams + strRetTail
 
 def getFileName(strName):
     return strName+'.py'
 
-class oclPyObjGenerator:
-    def __init__(self, strName):
+class OCLPyObjGenerator:
+    def __init__(self, strName, lstTypes, lstFuncs):
         self.className = strName
+        self.lstTypes = lstTypes
+        self.lstFuncs = lstFuncs
         pass
 
     def generateOCLPyObj(self):
@@ -29,3 +51,38 @@ class oclPyObjGenerator:
         with open(getFileName(self.className), 'w') as fPyObj:
             fPyObj.write(getClassDef(self.className))
         pass
+
+# dicArgd = {'var' : ['a', 'b', ...],
+#            'var_default' : [{'c':pyobj}],
+#            'args' : False/True,
+#            'argd' : False/True}
+
+lstTestMethodData = [
+dicTest0 = {'var' : [],
+            'var_default' : [],
+            'args' : False,
+            'argd' : False}
+dicTest1 = {'var' : ['a', 'b'],
+            'var_default' : [],
+            'args' : False,
+            'argd' : False}
+dicTest2 = {'var' : [],
+            'var_default' :[{'a':1, 'b':[2,3], 'c':{'d':6}, 'd':None}],
+            'args' : False,
+            'argd' : False}
+dicTest3 = {'var' : [],
+            'var_default' :[],
+            'args' : True,
+            'argd' : False}
+dicTest4 = {'var' : [],
+            'var_default' : [],
+            'args' : False,
+            'argd' : True}
+dicTest5 = {'var' : dicTest1['var'],
+            'var_default' : dicTest2['var_default'],
+            'args' : dicTest3['args'],
+            'argd' : dicTest4['argd']}]
+def testMethodDef():
+    for idx in xrange(len(lstTestMethodData)):
+        dicData = eval('dicTest'+str(idx))
+        print getMethodDef(str(idx), dicData)
